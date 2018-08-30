ff10f4bf62be08781c9947d8f8d76c80b54f6e29
debris <marek.kotewicz@gmail.com>
2018-08-30 16:43:49 +0200

    new event generation for ethabi-derive

--- derive/src/event.rs
+++ derive/src/event.rs
@@ -1,13 +1,17 @@
 use {quote, syn, ethabi};
 use heck::{SnakeCase, CamelCase};
 
-use super::{rust_type, to_syntax_string, from_token};
+use super::{rust_type, to_syntax_string, from_token, get_template_names, to_token};
 
 pub struct Event {
 	name: String,
 	log_fields: Vec<quote::Tokens>,
 	recreate_inputs_quote: quote::Tokens,
 	log_init: Vec<quote::Tokens>,
+	wildcard_filter_params: Vec<quote::Tokens>,
+	filter_declarations: Vec<quote::Tokens>,
+	filter_definitions: Vec<quote::Tokens>,
+	filter_init: Vec<quote::Tokens>,
 	anonymous: bool,
 }
 
@@ -44,6 +48,48 @@ impl<'a> From<&'a ethabi::Event> for Event {
 			.map(|(param_name, convert)| quote! { #param_name: #convert })
 			.collect();
 
+		let topic_kinds: Vec<_> = e.inputs
+			.iter()
+			.filter(|param| param.indexed)
+			.map(|param| rust_type(&param.kind))
+			.collect();
+		let topic_names: Vec<_> = e.inputs
+			.iter()
+			.enumerate()
+			.filter(|&(_, param)| param.indexed)
+			.map(|(index, param)| if param.name.is_empty() {
+				syn::Ident::from(format!("topic{}", index))
+			} else {
+				param.name.to_snake_case().into()
+			})
+			.collect();
+
+		// [T0, T1, T2]
+		let template_names: Vec<_> = get_template_names(&topic_kinds);
+
+		let filter_declarations: Vec<_> = topic_kinds.iter().zip(template_names.iter())
+			.map(|(kind, template_name)| quote! { #template_name: Into<ethabi::Topic<#kind>> })
+			.collect();
+
+		let filter_definitions: Vec<_> = topic_names.iter().zip(template_names.iter())
+			.map(|(param_name, template_name)| quote! { #param_name: #template_name })
+			.collect();
+
+		// The number of parameters that creates a filter which matches anything.
+		let wildcard_filter_params: Vec<_> = filter_definitions.iter().map(|_| quote! { ethabi::Topic::Any })
+			.collect();
+
+		let filter_init: Vec<_> = topic_names.iter().zip(e.inputs.iter().filter(|p| p.indexed))
+			.enumerate()
+			.take(3)
+			.map(|(index, (param_name, param))| {
+				let topic = syn::Ident::from(format!("topic{}", index));
+				let i = quote! { i };
+				let to_token = to_token(&i, &param.kind);
+				quote! { #topic: #param_name.into().map(|#i| #to_token), }
+			})
+			.collect();
+
 		let event_inputs = &e.inputs.iter().map(|x| {
 			let name = &x.name;
 			let kind = to_syntax_string(&x.kind);
@@ -65,6 +111,10 @@ impl<'a> From<&'a ethabi::Event> for Event {
 			recreate_inputs_quote,
 			log_init,
 			anonymous: e.anonymous,
+			wildcard_filter_params,
+			filter_declarations,
+			filter_definitions,
+			filter_init,
 		}
 	}
 }
@@ -89,6 +139,10 @@ impl Event {
 		let recreate_inputs_quote = &self.recreate_inputs_quote;
 		let anonymous = &self.anonymous;
 		let log_init = &self.log_init;
+		let filter_init = &self.filter_init;
+		let filter_declarations = &self.filter_declarations;
+		let filter_definitions = &self.filter_definitions;
+		let wildcard_filter_params = &self.wildcard_filter_params;
 
 		quote! {
 			pub mod #name {
@@ -109,10 +163,18 @@ impl Event {
 					}
 				}
 
-				pub fn filter() -> ethabi::TopicFilter {
+
+				pub fn filter<#(#filter_declarations),*>(#(#filter_definitions),*) -> ethabi::TopicFilter {
+					let raw = ethabi::RawTopicFilter {
+						#(#filter_init)*
+						..Default::default()
+					};
+
+					self.event.filter(raw).expect(INTERNAL_ERR)
 				}
 
 				pub fn wildcard_filter() -> ethabi::TopicFilter {
+					self.filter(#(#wildcard_filter_params),*)
 				}
 
 				pub fn parse_log(log: ethabi::RawLog) -> ethabi::Result<super::logs::#camel_name> {
@@ -180,16 +242,86 @@ mod tests {
 					}
 				}
 
-				pub fn filter() -> ethabi::TopicFilter {
+				pub fn filter<>() -> ethabi::TopicFilter {
+					let raw = ethabi::RawTopicFilter {
+						..Default::default()
+					};
+
+					self.event.filter(raw).expect(INTERNAL_ERR)
 				}
 
 				pub fn wildcard_filter() -> ethabi::TopicFilter {
+					self.filter()
 				}
 
 				pub fn parse_log(log: ethabi::RawLog) -> ethabi::Result<super::logs::Hello> {
 					let e = Event::default();
 					let mut log = e.parse_log(log)?.params.into_iter();
 					let result = super::logs::Hello {};
+					Ok(result)
+				}
+			}
+		};
+
+		assert_eq!(expected, e.generate_event());
+	}
+
+	#[test]
+	fn test_event_with_one_input() {
+		let ethabi_event = ethabi::Event {
+			name: "one".into(),
+			inputs: vec![ethabi::EventParam {
+				name: "foo".into(),
+				kind: ethabi::ParamType::Address,
+				indexed: true
+			}],
+			anonymous: false,
+		};
+
+		let e = Event::from(&ethabi_event);
+
+		let expected = quote! {
+			pub mod one {
+				use ethabi;
+				use super::INTERNAL_ERR;
+
+				struct Event {
+					event: ethabi::Event,
+				}
+
+				impl Default for Event {
+					fn default() -> Self {
+						Event {
+							event: "One".into(),
+							inputs: vec![ethabi::EventParam {
+								name: "foo".to_owned(),
+								kind: ethabi::ParamType::Address,
+								indexed: true
+							}],
+							anonymous: false,
+						}
+					}
+				}
+
+				pub fn filter<T0: Into<ethabi::Topic<ethabi::Address>>>(foo: T0) -> ethabi::TopicFilter {
+					let raw = ethabi::RawTopicFilter {
+						topic0: foo.into().map(|i| ethabi::Token::Address(i)),
+						..Default::default()
+					};
+
+					self.event.filter(raw).expect(INTERNAL_ERR)
+				}
+
+				pub fn wildcard_filter() -> ethabi::TopicFilter {
+					self.filter(ethabi::Topic::Any)
+				}
+
+				pub fn parse_log(log: ethabi::RawLog) -> ethabi::Result<super::logs::One> {
+					let e = Event::default();
+					let mut log = e.parse_log(log)?.params.into_iter();
+					let result = super::logs::One {
+						foo: log.next().expect(INTERNAL_ERR).value.to_address().expect(INTERNAL_ERR)
+					};
 					Ok(result)
 				}
 			}
