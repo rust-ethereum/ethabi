88a8bd33bc92887ce0fbb58255db18d2d3f58fdb
folex <0xdxdy@gmail.com>
2019-01-27 10:13:28 +0300

    Derive Clone for RawLog, LogParam and Log

--- ethabi/src/log.rs
+++ ethabi/src/log.rs
@@ -18,7 +18,7 @@ pub trait ParseLog {
 }
 
 /// Ethereum log.
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 pub struct RawLog {
 	/// Indexed event params are represented as log topics.
 	pub topics: Vec<Hash>,
@@ -36,7 +36,7 @@ impl From<(Vec<Hash>, Bytes)> for RawLog {
 }
 
 /// Decoded log param.
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 pub struct LogParam {
 	/// Decoded log name.
 	pub name: String,
@@ -45,7 +45,7 @@ pub struct LogParam {
 }
 
 /// Decoded log.
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 pub struct Log {
 	/// Log params.
 	pub params: Vec<LogParam>,
