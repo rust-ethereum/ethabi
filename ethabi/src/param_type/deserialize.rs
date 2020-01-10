600de2fd2b7c74a3c3ab542c3c891b3c1fef8d7d
David Palm <dvdplm@gmail.com>
2020-01-10 10:04:39 +0100

    Add license headers

--- ethabi/src/param_type/deserialize.rs
+++ ethabi/src/param_type/deserialize.rs
@@ -1,3 +1,11 @@
+// Copyright 2015-2019 Parity Technologies
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
 use std::fmt;
 use serde::{Deserialize, Deserializer};
 use serde::de::{Error as SerdeError, Visitor};
