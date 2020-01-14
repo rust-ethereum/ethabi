e63feb5075ca24950cc8835fcc2adb67eecdd702
David <dvdplm@gmail.com>
2020-01-14 11:43:26 +0100

    Update cli/src/main.rs
    Co-Authored-By: Andronik Ordian <write@reusable.software>

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -296,7 +296,7 @@ mod tests {
 	fn int_encode_large_negative_numbers() {
 		// i256::min_value() is ok
 		let command = "ethabi encode params -v int256 -57896044618658097711785492504343953926634992332820282019728792003956564819968 --lenient".split(" ");
-		let expected = "8000000000000000000000000000000000000000000000000000000000000001";
+		let expected = "8000000000000000000000000000000000000000000000000000000000000000";
 		assert_eq!(execute(command).unwrap(), expected);
 
 		// i256::min_value() -1 is too much
