9121c221b9d83b7931022e47f526591801699652
Zac Burns <That3Percent@gmail.com>
2020-06-17 11:49:21 -0500

    Added StateMutability to Function

--- ethabi/src/lib.rs
+++ ethabi/src/lib.rs
@@ -25,6 +25,7 @@ mod operation;
 mod param;
 pub mod param_type;
 mod signature;
+mod state_mutability;
 pub mod token;
 mod tuple_param;
 mod util;
@@ -45,6 +46,7 @@ pub use crate::{
 	log::{Log, LogFilter, LogParam, ParseLog, RawLog},
 	param::Param,
 	param_type::ParamType,
+	state_mutability::StateMutability,
 	token::Token,
 	tuple_param::TupleParam,
 };
